682f791df102d7cbe832eebab43621d12b8d00e5
vcoronado2014 <vcoronado.alarcon@gmaiil.com>
2021-10-22 17:33:20 -0300

    Se quita la página productos ccomo pagina adicional, se agrega dicha pagina como cccompnente en el home

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   pre_production: true,
   //API_ENDPOINT: "https://www.asambleas.cl/apps_gaj/api/",
   API_ENDPOINT: '/apps/api/',
-  URL_FOTOS:'http://gajico.asambleas.cl/apps/Repositorio/',
+  URL_FOTOS:'http://www.gajico.cl/apps/Repositorio/',
   DIRECCION: 'Merced 480 Piso 2, Santiago - Chile',
   TELEFONO:'(+56 2) 2588 8800',
   CONTACTO:'contacto@rayensalud.com',
